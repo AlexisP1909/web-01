eee190edbc3613beb6e5cd697edb9a5fe93aece7
AlexisP1909 <alexis.pouillieute@gmail.com>
2024-03-05 10:01:11 +0100

    fixeďgitignore + Classes

---
+++ front-end/src/main.js
@@ -0,0 +1,29 @@
+// TODO #import-router: use ES named imports to import the router
+import {Router} from "./app/scripts/router";
+// TODO #import-components: use ES named imports to import WelcomeComponent, GameComponent a ScoreComponent
+import {WelcomeComponent} from "./app/scripts/welcome";
+import {GameComponent} from "./app/scripts/game";
+import {ScoreComponent} from "./app/scripts/score";
+// TODO #import-css: use ES side-effect imports to import styles/style.css
+import "/node_modules/bootstrap/dist/css/bootstrap.css"
+import "./app/styles/style.css";
+
+const outlet = document.querySelector("#content-outlet");
+const router = new Router(outlet);
+router
+  .register("", {
+    component: WelcomeComponent,
+    // TODO #import-html: remove the templateUrl property.
+  })
+  .register("welcome", {
+    component: WelcomeComponent,
+    // TODO #import-html: remove the templateUrl property.
+  })
+  .register("game", {
+    component: GameComponent,
+    // TODO #import-html: remove the templateUrl property.
+  })
+  .register("score", {
+    component: ScoreComponent,
+    // TODO #import-html: remove the templateUrl property.
+  });
